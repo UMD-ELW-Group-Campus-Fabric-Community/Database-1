b68af14a170ba1a679024967e501b7124f8dafd3
John Murray <69658747+lovendatj@users.noreply.github.com>
2022-04-04 11:30:53 -0400

    updated test data

--- sql/dummy_data.sql
+++ sql/dummy_data.sql
@@ -9,3 +9,14 @@ VALUES ('admin', 'admin', 'test@bob.com', '1234567890', 1, 'admin');
 -- Create Impropper User
 -- INSERT INTO User_Information (username, user_password, user_email, user_phone, organization_id, user_level)
 -- VALUES ('admin', 'admin', 'not-an-email', '1234567890', 1, 'admin');
+
+-- Create Department
+INSERT INTO Department (department_name, organization_id)
+VALUES ('Bob', 1);
+
+-- Create Program_Information
+INSERT INTO Program_Information (program_name, program_description, program_website, program_focus, organization_id)
+VALUES ('Bob', 'Bob', 'www.bob.com', '{
+    "Programming",
+    "Math"
+}', 1);
